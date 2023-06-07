7839169ece0bbb2ef1c45da2208d602aa0c7f213
Maria Litwa <contact@marialitwa.com>
2023-06-07 16:35:17 +0200

    Add basic styles and media queries to Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
-// import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg"
 import "./navbar.css";
 
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
@@ -21,6 +24,24 @@ const Navbar = () => {
       <div className="gpt3__navbar-sign">
         <p>Sign in</p>
         <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu
+        ?  <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+        :  <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center ">
+            <div className="gpt3__navbar-menu_container-links">
+              <p><a href="#home">Home</a></p>
+              <p><a href="#wgpt3">What is GPT3?</a></p>
+              <p><a href="#possibility">Open Ai</a></p>
+              <p><a href="#features">Case Studies</a></p>
+              <p><a href="#blog">Library</a></p>
+            </div>
+          </div>
+        )
+        }
       </div>
     </div>
   )
